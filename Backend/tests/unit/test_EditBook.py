9bbf529ab6a97967a3ef9ffe04787bf11530ed3a
Abisai-Olivares <nulsiona500@icloud.com>
2024-06-06 01:42:12 -0600

    Acualizacion de libro

---
+++ Backend/tests/unit/test_EditBook.py
@@ -0,0 +1,19 @@
+import json
+
+from Backend.book.EditBook.EditBook import lambda_handler
+
+
+def test_lambda_post():
+    mock = {
+        'body': json.dumps({
+            'idbook':'1',
+            'titulo': 'EL gordito tiste',
+            'fecha_publicacion': '2024-06-07',
+            'autor': 'SAMUEL',
+            'editorial': 'La casa de los sueños',
+            'status': '0'
+        })
+    }
+
+    regreso = lambda_handler(mock)
+    print(regreso)
